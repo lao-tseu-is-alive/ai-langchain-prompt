665cb146f53689501b8d7e72a7dc58b5ecfb560e
Carlos GIL <lao.tseu.is.alive@gmail.com>
2024-12-04 15:22:04 +0100

    adding simple bash script for checking X.ai is working

---
+++ my_xai_experiments/00_check_xai_api_key_is_ok.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+ENV_FILENAME='.env'
+if [ -f $ENV_FILENAME ]; then
+  set -a
+    # shellcheck disable=SC1090
+    source <(sed -e '/^#/d;/^\s*$/d' -e "s/'/'\\\''/g" -e "s/=\(.*\)/='\1'/g" $ENV_FILENAME )
+    echo "Checking if your XAI_API_KEY env is ok... XAI_API_KEY:${XAI_API_KEY}"
+    curl https://api.x.ai/v1/chat/completions \
+      -H "Content-Type: application/json" \
+      -H "Authorization: Bearer $XAI_API_KEY" \
+  -d '{
+        "messages": [
+          {
+            "role": "system",
+            "content": "You are Grok, a chatbot inspired by the Hitchhikers Guide to the Galaxy."
+          },
+          {
+            "role": "user",
+            "content": "What is the meaning of life, the universe, and everything?"
+          }
+        ],
+        "model": "grok-beta",
+        "stream": false,
+        "temperature": 0
+      }' |jq
+    set +a
+    echo "XAI_API_KEY is ok."
+fi
